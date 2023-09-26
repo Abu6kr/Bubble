aac2034822231ecaca31accb4538d652dce5f5c6
abo <abubker630a@gmail.com>
2023-09-26 14:22:58 +0500

    Initial Commit Bubble app

---
+++ Bubble/BubbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleApp.swift
+//  Bubble
+//
+//  Created by Abobakr Al Zain  on 26.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BubbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
