85241c5fc966c1c953a137bf242761f0eb3adfb9
abo <abubker630a@gmail.com>
2023-10-17 15:20:43 +0500

    make new cutems bar withe  Images proies and add to views

---
+++ Bubble/Core/Components/CustemsHeaderBar.swift
@@ -0,0 +1,18 @@
+//
+//  CustemsHeaderBar.swift
+//  Bubble
+//
+//  Created by Abobakr Al Zain  on 17.10.2023.
+//
+
+import SwiftUI
+
+struct CustemsHeaderBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustemsHeaderBar()
+}
